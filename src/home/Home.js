0d2d22341400a2affdb97545ffccbdaea635881a
Gega <latariagega1@gmail.com>
2022-10-13 18:47:51 +0400

    adding about container

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { useRef } from "react";
 import "./Home.css";
 
 export default function Home() {
+  const ref = useRef(null);
   const name = "Gega Lataria";
+  const handleClick = () => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div>
       <div className="home-page">
@@ -11,8 +17,15 @@ export default function Home() {
             Hi, My name is <span className="name-container__input">{name}</span>
           </h1>
           <h1>I'm a React Developer</h1>
+          <button className="name-container__button" onClick={handleClick}>
+            Know More
+          </button>
         </div>
       </div>
+      <div className="about-container" ref={ref}>
+        About
+      </div>
+      <div className="projects-container">projects</div>
     </div>
   );
 }
