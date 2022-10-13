ad6470b59090381349257eb7ab89c9a0d745050f
Gega <latariagega1@gmail.com>
2022-10-13 19:43:02 +0400

    updating about container

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useRef } from "react";
+import profile from "../photos/profile.jpg";
 import "./Home.css";
 
 export default function Home() {
@@ -16,14 +17,31 @@ export default function Home() {
           <h1>
             Hi, My name is <span className="name-container__input">{name}</span>
           </h1>
-          <h1>I'm a React Developer</h1>
+          <h1>I'm a React Developer🚀</h1>
           <button className="name-container__button" onClick={handleClick}>
             Know More
           </button>
         </div>
       </div>
       <div className="about-container" ref={ref}>
-        About
+        <h1 className="about-container__title">About</h1>
+        <div className="about-container__flex">
+          <img
+            src={profile}
+            alt="profile"
+            className="about-container__image"
+          ></img>
+          <div>
+            <p className="about-container__input">
+              I'm self-starting, hard-working React Developer.
+            </p>
+            <br />
+            <p className="about-container__input">
+              To see my Resume, click the button below.
+            </p>
+            <button className="about-container__button">View Resume</button>
+          </div>
+        </div>
       </div>
       <div className="projects-container">projects</div>
     </div>
