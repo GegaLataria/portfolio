280d1205685d2dc83bdf2545f0070a94603e6536
Gega <latariagega1@gmail.com>
2022-10-15 17:50:11 +0400

    updating projects

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -12,16 +12,17 @@ export default function Projects() {
         <div className="project-container__flex">
           <div className="project-input-container">
             <h2 className="project-title">Forwarder Web</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a
-              libero ornare, ornare lorem id, mollis ex. Nulla facilisi. Etiam
-              placerat gravida quam quis iaculis. Duis mattis cursus elit, sit
-              amet mollis augue feugiat sed. Donec lacus orci, eleifend a rutrum
-              sit amet, volutpat et felis. Duis finibus volutpat lectus, at
-              euismod ante rhoncus eu. Morbi ac auctor ex. Phasellus feugiat
-              neque nunc, et aliquam magna posuere molestie. Nulla dignissim
-              vitae ante ut tincidunt. Vivamus placerat a velit id semper.
-            </p>
+            <ul className="project-description">
+              <li>Worked on Front-end using React.js</li>
+              <li>Responsive on all screen sizes using CSS</li>
+              <li>Used React Context to share data to all components</li>
+              <li>Used Axios to fetch data from server</li>
+              <li>Used React Router to configure routes</li>
+              <li>Used Google Maps API to load company address</li>
+              <li>Used iframe to load YouTube video on a website</li>
+              <li>Implemented Login by saving data in local storage</li>
+              <li>Implemented Register by saving user data in local storage</li>
+            </ul>
             <a
               className="see-live-button"
               target="_blank"
