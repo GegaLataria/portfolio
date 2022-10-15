92c091a6559e4fe8d9beeaff31713b2c232c11b7
Gega <latariagega1@gmail.com>
2022-10-15 18:16:06 +0400

    updated projects component

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -20,8 +20,9 @@ export default function Projects() {
               <li>Used React Router to configure routes</li>
               <li>Used Google Maps API to load company address</li>
               <li>Used iframe to load YouTube video on a website</li>
-              <li>Implemented Login by saving data in local storage</li>
-              <li>Implemented Register by saving user data in local storage</li>
+              <li>
+                Implemented Login and Register by saving data in local storage
+              </li>
             </ul>
             <a
               className="see-live-button"
@@ -45,16 +46,14 @@ export default function Projects() {
         <div className="project-container__flex">
           <div className="project-input-container">
             <h2 className="project-title">Online Shop</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a
-              libero ornare, ornare lorem id, mollis ex. Nulla facilisi. Etiam
-              placerat gravida quam quis iaculis. Duis mattis cursus elit, sit
-              amet mollis augue feugiat sed. Donec lacus orci, eleifend a rutrum
-              sit amet, volutpat et felis. Duis finibus volutpat lectus, at
-              euismod ante rhoncus eu. Morbi ac auctor ex. Phasellus feugiat
-              neque nunc, et aliquam magna posuere molestie. Nulla dignissim
-              vitae ante ut tincidunt. Vivamus placerat a velit id semper.
-            </p>
+            <ul className="project-description">
+              <li>Worked on Front-end using React.js</li>
+              <li>Used React Context to share data to all components</li>
+              <li>Used React Router to configure routes</li>
+              <li>Implemented Cart by saving data in local storage</li>
+              <li>Used setTimeout to implement auto-updating slideshow</li>
+              <li>Responsive on all screen sizes using CSS</li>
+            </ul>
             <a
               className="see-live-button"
               target="_blank"
@@ -77,16 +76,15 @@ export default function Projects() {
         <div className="project-container__flex">
           <div className="project-input-container">
             <h2 className="project-title">Optimo Group</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a
-              libero ornare, ornare lorem id, mollis ex. Nulla facilisi. Etiam
-              placerat gravida quam quis iaculis. Duis mattis cursus elit, sit
-              amet mollis augue feugiat sed. Donec lacus orci, eleifend a rutrum
-              sit amet, volutpat et felis. Duis finibus volutpat lectus, at
-              euismod ante rhoncus eu. Morbi ac auctor ex. Phasellus feugiat
-              neque nunc, et aliquam magna posuere molestie. Nulla dignissim
-              vitae ante ut tincidunt. Vivamus placerat a velit id semper.
-            </p>
+            <ul className="project-description">
+              <li>Worked on Front-end using Next.js</li>
+              <li>Used server to fetch info about employees</li>
+              <li>App can filter employees by location and job position</li>
+              <li>
+                Used getStaticPaths to pre-render pages that use dynamic routes
+              </li>
+              <li>Used getStaticProps to fetch data</li>
+            </ul>
             <a
               className="see-live-button"
               target="_blank"
